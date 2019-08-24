802eacc9f1d7943a0c3afac71a6d19fd5c00e4ff
Emanuel Freitas <efreitasn@hotmail.com>
2019-08-24 00:20:49 -0300

    Add done section

--- cmd/go-todo/handlers/todo.go
+++ cmd/go-todo/handlers/todo.go
@@ -22,16 +22,24 @@ type Todo struct {
 // List list all todos.
 func (t *Todo) List(w http.ResponseWriter, r *http.Request) {
 	templateData := struct {
-		Todos        []todo.Todo
+		TodosDone    []todo.Todo
+		TodosNotDone []todo.Todo
+		HasTodos     bool
 		FlashMessage *flash.Message
 	}{}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cursor, err := t.c.Find(
+	// Done
+	cursorDone, err := t.c.Find(
 		ctx,
-		bson.D{},
+		bson.D{
+			{
+				Key:   "done",
+				Value: true,
+			},
+		},
 		&options.FindOptions{
 			Sort: bson.D{
 				{
@@ -59,11 +67,11 @@ func (t *Todo) List(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var todos []todo.Todo
+	var todosDone []todo.Todo
 
-	err = cursor.All(
+	err = cursorDone.All(
 		context.Background(),
-		&todos,
+		&todosDone,
 	)
 
 	if err != nil {
@@ -83,7 +91,69 @@ func (t *Todo) List(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	templateData.Todos = todos
+	// Not done
+	cursorNotDone, err := t.c.Find(
+		ctx,
+		bson.D{
+			{
+				Key:   "done",
+				Value: false,
+			},
+		},
+		&options.FindOptions{
+			Sort: bson.D{
+				{
+					Key:   "createdAt",
+					Value: 1,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		flashMsgCookie := flash.Read(w, r)
+
+		if flashMsgCookie != nil {
+			templateData.FlashMessage = flashMsgCookie
+		} else {
+			templateData.FlashMessage = &flash.Message{
+				Kind:    1,
+				Content: "Error while fetching the list of todos.",
+			}
+		}
+
+		utils.WriteTemplates(w, templateData, "todos")
+
+		return
+	}
+
+	var todosNotDone []todo.Todo
+
+	err = cursorNotDone.All(
+		context.Background(),
+		&todosNotDone,
+	)
+
+	if err != nil {
+		flashMsgCookie := flash.Read(w, r)
+
+		if flashMsgCookie != nil {
+			templateData.FlashMessage = flashMsgCookie
+		} else {
+			templateData.FlashMessage = &flash.Message{
+				Kind:    1,
+				Content: "Error while fetching the list of todos.",
+			}
+		}
+
+		utils.WriteTemplates(w, templateData, "todos")
+
+		return
+	}
+
+	templateData.TodosDone = todosDone
+	templateData.TodosNotDone = todosNotDone
+	templateData.HasTodos = len(todosDone) != 0 || len(todosNotDone) != 0
 	templateData.FlashMessage = flash.Read(w, r)
 
 	utils.WriteTemplates(w, templateData, "todos")
